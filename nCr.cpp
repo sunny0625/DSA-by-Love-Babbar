700a2f9359b6a07596864bb388d9d57814a0e293
Sukhbir Singh <sukhbir2506@outlook.com>
2023-04-16 23:35:20 +0530

    Love Babbar

---
+++ nCr.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int factorial(int n)
+{
+    int fact = 1;
+    for(int i = 1; i <= n; i++)
+    fact = fact * i;
+    return fact;
+}
+int nCr(int n, int r)
+{
+    int num = factorial(n);
+    int denom = factorial(r) * factorial(n - r);
+    return num / denom;
+}
+int main()
+{
+    int n, r;
+    cout << "Enter value for n: ";
+    cin >> n;
+    cout << "Enter value for r: ";
+    cin >> r;
+    factorial(n);
+    cout << "Factorial of " << n << " C " << r << " is: " << nCr(n, r);
+}
